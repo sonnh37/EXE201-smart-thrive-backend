4c5078e9f499cf29b1df7353ea3a42295ec4b368
sonnh37 <nguyenson20zzzz@gmail.com>
2024-08-29 09:14:12 +0700

    test

--- EXE201-smart-thrive/EXE201.SmartThrive.Domain/Configs/Mappings/MappingProfile.Subject.cs
+++ EXE201-smart-thrive/EXE201.SmartThrive.Domain/Configs/Mappings/MappingProfile.Subject.cs
@@ -10,6 +10,7 @@ public partial class MappingProfile : Profile
     private void SubjectMapping()
     {
         CreateMap<Subject, SubjectResult>().ReverseMap();
+        CreateMap<Subject, SubjectResult>();
         CreateMap<Subject, SubjectCreateCommand>().ReverseMap();
         CreateMap<Subject, SubjectUpdateCommand>().ReverseMap();
     }
