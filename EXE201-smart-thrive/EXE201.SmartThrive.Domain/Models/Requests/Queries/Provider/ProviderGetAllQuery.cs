9483c0fd715d18b7893d26a84a46d21f8e561e2a
Thanh Thu <thanhthu26082003@gmail.com>
2024-08-30 21:41:46 +0700

    userFilter

---
+++ EXE201-smart-thrive/EXE201.SmartThrive.Domain/Models/Requests/Queries/Provider/ProviderGetAllQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE201.SmartThrive.Domain.Models.Requests.Queries.Provider
+{
+    public class ProviderGetAllQuery: PagedQuery
+    {
+        public Guid? UserId { get; set; }
+
+        public string? CompanyName { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Website { get; set; }
+    }
+}
