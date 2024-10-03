fcac292858a60cdf97f07ba830aeb358cb77dd11
Thanh Thu <thanhthu26082003@gmail.com>
2024-10-03 11:02:09 +0700

    getSession

--- EXE201-smart-thrive/EXE201.SmartThrive.Data/Migrations/20241001134351_editSession.Designer.cs
+++ EXE201-smart-thrive/EXE201.SmartThrive.Data/Migrations/20241001134351_editSession.Designer.cs
@@ -4,6 +4,7 @@ using EXE201.SmartThrive.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EXE201.SmartThrive.Data.Migrations
 {
     [DbContext(typeof(STDbContext))]
-    partial class STDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241001134351_editSession")]
+    partial class editSession
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
