00c7e9d7a29e188b2d349f02e72a844044be6d73
loctodale <thang336655@gmail.com>
2024-10-03 14:50:05 +0700

    update session service

--- EXE201-smart-thrive/EXE201.SmartThrive.Domain/Contracts/Services/ISessionService.cs
+++ EXE201-smart-thrive/EXE201.SmartThrive.Domain/Contracts/Services/ISessionService.cs
@@ -1,12 +1,15 @@
 ﻿using EXE201.SmartThrive.Domain.Contracts.Bases;
 using EXE201.SmartThrive.Domain.Entities;
 using EXE201.SmartThrive.Domain.Models.Requests.Commands.Session;
+using EXE201.SmartThrive.Domain.Models.Responses;
+using EXE201.SmartThrive.Domain.Models.Results;
 
 namespace EXE201.SmartThrive.Domain.Contracts.Services;
 
 public interface ISessionService : IBaseService
 {
-    Task<Session> CreateSession(string type, SessionCreateCommand payload);
-    Task<Session> UpdateSession(string type, SessionUpdateCommand payload);
-    Task DeleteSession(Guid sessionId);
+    Task<BusinessResult> GetById(Guid id);
+    Task<BusinessResult> CreateSession(string type, SessionCreateCommand payload);
+    Task<BusinessResult> UpdateSession(string type, SessionUpdateCommand payload);
+    Task<BusinessResult> DeleteSession(Guid sessionId);
 }
